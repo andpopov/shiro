3c8f3500a587f6afaec870067d9ed3c901dde3da
Andrey Popov <andpopov@gmail.com>
2022-12-29 16:56:26 +0300

    INI configuration

--- shiro_research/src/main/java/com/github/andpopov/shiro/Main.java
+++ shiro_research/src/main/java/com/github/andpopov/shiro/Main.java
@@ -1,7 +1,24 @@
 package com.github.andpopov.shiro;
 
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.util.Factory;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println();
+    public static void main(String[] args) throws InterruptedException {
+        // Load the INI configuration
+        final Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject currentUser = SecurityUtils.getSubject();
+        if(!currentUser.isAuthenticated()) {
+            currentUser.login(new UsernamePasswordToken("user1", "password"));
+        }
+
+        System.out.println(currentUser.isAuthenticated());
     }
 }
